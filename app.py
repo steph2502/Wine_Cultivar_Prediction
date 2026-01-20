6ce40bfe30499b2bb57af5ba75fc4284109a5cf9
Stephanie✨ <127240541+steph2502@users.noreply.github.com>
2026-01-20 02:44:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import numpy as np
-import joblib
+import pickle
 
 app = Flask(__name__)
 
 # Load model and scaler
-model = joblib.load("model/wine_cultivar_model.pkl")
-scaler = joblib.load("model/scaler.pkl")
+with open("model/wine_cultivar_model.pkl", "rb") as file:
+    model, scaler = pickle.load(file)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -17,16 +17,17 @@ def home():
         malic_acid = float(request.form["malic_acid"])
         ash = float(request.form["ash"])
         alcalinity = float(request.form["alcalinity"])
-        total_phenols = float(request.form["total_phenols"])
         flavanoids = float(request.form["flavanoids"])
+        color_intensity = float(request.form["color_intensity"])
 
-        features = np.array([[alcohol, malic_acid, ash, alcalinity, total_phenols, flavanoids]])
+        features = np.array([[alcohol, malic_acid, ash, alcalinity, flavanoids, color_intensity]])
         features_scaled = scaler.transform(features)
 
-        pred = model.predict(features_scaled)[0]
-        prediction = f"Cultivar {pred + 1}"
+        result = model.predict(features_scaled)[0]
+        prediction = f"Cultivar {result + 1}"
 
     return render_template("index.html", prediction=prediction)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
